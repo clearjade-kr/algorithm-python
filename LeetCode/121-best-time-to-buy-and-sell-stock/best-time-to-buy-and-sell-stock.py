0339cf4ad7e4773366c2dace6587dfae105ccee7
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2024-04-07 21:27:24 +0900

    Time: 687 ms (92.29%) | Memory: 27.4 MB (34.85%) - LeetSync

---
+++ LeetCode/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        cur_min = prices[0]
+        cur_profit = 0
+        
+        for val in prices:
+            if cur_min > val:
+                cur_min = val
+            elif val - cur_min > cur_profit:
+                cur_profit = val - cur_min
+                
+        return cur_profit
