b712fdb4a133d7b8533ddfac2c155e656d764b8f
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2024-04-07 23:31:36 +0900

    Time: 392 ms (23.83%) | Memory: 17.9 MB (23.43%) - LeetSync

---
+++ LeetCode/55-jump-game/jump-game.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # Seems like using dynamic programming
+        flag_reach = [False] * len(nums)
+        flag_reach[0] = True
+        cur_idx = 0
+        for i in range(len(nums)):
+            if cur_idx >= len(nums):
+                break
+            jump_height = nums[i]
+            if cur_idx >= i + jump_height:
+                continue
+            for j in range(cur_idx + 1, i + jump_height + 1):
+                if j >= len(nums):
+                    break
+                flag_reach[j] = flag_reach[i]
+            cur_idx = i + jump_height
+
+        print(flag_reach)
+        return flag_reach[-1]
