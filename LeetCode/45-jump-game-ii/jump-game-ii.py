0613bc0f12e2eff49addcab4fb22a965fcb57cf8
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2024-04-08 23:34:29 +0900

    Time: 111 ms (46.49%) | Memory: 17.6 MB (46.49%) - LeetSync

--- LeetCode/45-jump-game-ii/jump-game-ii.py
+++ LeetCode/45-jump-game-ii/jump-game-ii.py
@@ -1,15 +1,31 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        dp = [-1] * len(nums)
-        dp[0] = 0
+        # -- My previous solution --
+        # dp = [-1] * len(nums)
+        # dp[0] = 0
+        #
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, i + nums[i] + 1):
+        #         if j >= len(nums):
+        #             break
+        #         elif dp[j] != -1:
+        #             continue
+        #         dp[j] = dp[i] + 1
+        #
+        # # print(dp)
+        # return dp[-1]
 
-        for i in range(len(nums)):
-            for j in range(i + 1, i + nums[i] + 1):
-                if j >= len(nums):
-                    break
-                elif dp[j] != -1:
-                    continue
-                dp[j] = dp[i] + 1
+        # -- Better solution found --
+        # IDEA : find the furthest position that I can get from current position
+        # If I can reach specific position,
+        # then I can reach any position before at same number of jump.
+        jumps = 0
+        end = 0
 
-        # print(dp)
-        return dp[-1]
+        reach = 0
+        for pos in range(len(nums) - 1):
+            reach = max(reach, pos + nums[pos])
+            if pos == end:
+                end = reach
+                jumps += 1
+        return jumps
