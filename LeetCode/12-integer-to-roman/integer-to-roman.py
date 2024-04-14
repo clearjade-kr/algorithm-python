b9c50e1a5092b16ac2f6215be9afedabf2c3edce
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2024-04-14 21:24:20 +0900

    Time: 51 ms (36.48%) | Memory: 16.6 MB (63.61%) - LeetSync

---
+++ LeetCode/12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,26 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        dict_roman = {
+            1: 'I',
+            4: 'IV',
+            5: 'V',
+            9: 'IX',
+            10: 'X',
+            40: 'XL',
+            50: 'L',
+            90: 'XC',
+            100: 'C',
+            400: 'CD',
+            500: 'D',
+            900: 'CM',
+            1000: 'M'
+        }
+
+        ans = ''
+        keys = sorted(dict_roman.keys(), reverse=True)
+        for v in keys:
+            if num >= v:
+                cnt_v = num // v
+                ans = ans + (dict_roman[v] * cnt_v)
+                num -= v * cnt_v
+        return ans
