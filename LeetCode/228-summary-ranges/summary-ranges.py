b6d84ee94586856eb755224eb5a1e0ef01f7d817
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2024-05-11 21:27:36 +0900

    Time: 42 ms (8.82%) | Memory: 16.6 MB (45.44%) - LeetSync

--- LeetCode/228-summary-ranges/summary-ranges.py
+++ LeetCode/228-summary-ranges/summary-ranges.py
@@ -5,10 +5,7 @@ class Solution:
         start_idx = 0
         cur_idx = 0
         while cur_idx < len(nums):
-            if cur_idx < len(nums) - 1 and nums[cur_idx + 1] == nums[cur_idx] + 1:
-                # continue
-                pass
-            else:
+            if not (cur_idx < len(nums) - 1 and nums[cur_idx + 1] == nums[cur_idx] + 1):
                 if cur_idx - start_idx >= 1:
                     ret.append('%d->%d' % (nums[start_idx], nums[cur_idx]))
                 else:
