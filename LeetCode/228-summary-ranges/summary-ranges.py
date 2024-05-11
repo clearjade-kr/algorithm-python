5472f1b484430b77bd6a1274df42c46e38f17ac5
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2024-05-11 21:26:15 +0900

    Time: 37 ms (38.98%) | Memory: 16.6 MB (9.62%) - LeetSync

---
+++ LeetCode/228-summary-ranges/summary-ranges.py
@@ -0,0 +1,19 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        ret = []
+
+        start_idx = 0
+        cur_idx = 0
+        while cur_idx < len(nums):
+            if cur_idx < len(nums) - 1 and nums[cur_idx + 1] == nums[cur_idx] + 1:
+                # continue
+                pass
+            else:
+                if cur_idx - start_idx >= 1:
+                    ret.append('%d->%d' % (nums[start_idx], nums[cur_idx]))
+                else:
+                    ret.append('%d' % nums[cur_idx])
+                start_idx = cur_idx + 1
+            cur_idx += 1
+
+        return ret
