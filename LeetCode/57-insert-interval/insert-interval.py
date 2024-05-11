d9e39af42a19c2e4a3eeb07c377207fd11190a2c
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2024-05-11 22:19:58 +0900

    Time: 88 ms (6.23%) | Memory: 19.9 MB (37.43%) - LeetSync

---
+++ LeetCode/57-insert-interval/insert-interval.py
@@ -0,0 +1,19 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.sort()
+        target_it = newInterval.copy()
+        start_idx = -1
+        end_idx = len(intervals)
+        for i in range(len(intervals)):
+            it = intervals[i]
+            if it[1] < newInterval[0]:
+                start_idx = i
+                continue
+            elif it[0] > newInterval[1]:
+                end_idx = i
+                break
+            else:
+                target_it[0] = min(target_it[0], it[0])
+                target_it[1] = max(target_it[1], it[1])
+
+        return intervals[:start_idx + 1] + [target_it] + intervals[end_idx:]
