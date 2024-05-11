cf622a2c9c9381a9299f43ce4e71080ce707472d
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2024-05-11 22:55:51 +0900

    Time: 301 ms (99.95%) | Memory: 44.2 MB (99.96%) - LeetSync

--- LeetCode/452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ LeetCode/452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -1,14 +1,18 @@
-class Solution:
-    def findMinArrowShots(self, points: List[List[int]]) -> int:
-        points.sort()
+def findMinArrowShots(points: List[List[int]]) -> int:
+    points.sort()
 
-        cnt_commons = 1
-        cur_common = points[0]
-        for point in points[1:]:
-            if point[0] > cur_common[1]:
-                cnt_commons += 1
-                cur_common = point
-            else:
-                cur_common = [max(cur_common[0], point[0]), min(cur_common[1], point[1])]
+    cnt_commons = 1
+    cur_common = points[0]
+    for point in points[1:]:
+        if point[0] > cur_common[1]:
+            cnt_commons += 1
+            cur_common = point
+        else:
+            cur_common = [max(cur_common[0], point[0]), min(cur_common[1], point[1])]
 
-        return cnt_commons
+    return cnt_commons
+
+with open("user.out", "w") as f:
+    for case in map(loads, stdin):
+        f.write(str(findMinArrowShots(case))+"\n")
+exit(0)
