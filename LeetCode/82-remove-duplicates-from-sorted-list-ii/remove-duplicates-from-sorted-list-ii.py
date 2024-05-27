7cc29f5cdc8917bd4caaeca8719da141232d93d1
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2024-05-28 00:14:18 +0900

    Time: 35 ms (83.30%) | Memory: 16.6 MB (13.35%) - LeetSync

--- LeetCode/82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
+++ LeetCode/82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -2,41 +2,19 @@ class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
+        
+        dummy = ListNode()
+        dummy.next = head
 
-        ret_node = head
-
-        prev_node = None
-        cur_node = head
-        next_node = head.next
-        target_val = None
-
-        while cur_node:
-            if next_node and cur_node.val == next_node.val:
-                if prev_node:
-                    prev_node.next = next_node
-                else:
-                    ret_node = next_node
-                target_val = cur_node.val
-                cur_node = next_node
-                if cur_node:
-                    next_node = cur_node.next
-
-            elif cur_node.val == target_val:
-                if prev_node:
-                    prev_node.next = next_node
-                else:
-                    ret_node = next_node
-                cur_node = next_node
-                if cur_node:
-                    next_node = cur_node.next
+        cur_node = dummy
+        while cur_node.next and cur_node.next.next:
+            if cur_node.next.val == cur_node.next.next.val:
+                while cur_node.next and cur_node.next.next \
+                        and cur_node.next.val == cur_node.next.next.val:
+                    cur_node.next = cur_node.next.next
+                cur_node.next = cur_node.next.next
 
             else:
-                if next_node:
-                    target_val = cur_node.val
-                else:
-                    break
-                prev_node = cur_node
-                cur_node = next_node
-                next_node = cur_node.next
+                cur_node = cur_node.next
 
-        return ret_node
+        return dummy.next
