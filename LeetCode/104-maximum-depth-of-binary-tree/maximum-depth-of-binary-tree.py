8e583b03a3a0f303246452d1ba3d09a5058d2877
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2024-05-30 23:20:28 +0900

    Time: 42 ms (43.40%) | Memory: 17.5 MB (82.47%) - LeetSync

---
+++ LeetCode/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        ret_val = 0
+        q = [(1 , root)]
+        while q:
+            cur_h, cur_node = q.pop()
+            if not cur_node.left and not cur_node.right:
+                ret_val = max(ret_val, cur_h)
+                continue
+
+            if cur_node.left:
+                q.append((cur_h + 1, cur_node.left))
+            if cur_node.right:
+                q.append((cur_h + 1, cur_node.right))
+
+        return ret_val
