39dc7901eb1bbf8870af5a0fddbbbd4c16f54b87
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2024-06-08 13:14:02 +0900

    Time: 65 ms (49.86%) | Memory: 21 MB (41.40%) - LeetSync

--- LeetCode/124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ LeetCode/124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -5,19 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.max_val = -10000
+
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        max_val = -10000
 
         def traverse_path(node):
-            nonlocal max_val
             if not node:
                 return 0
             left_val = traverse_path(node.left)
             right_val = traverse_path(node.right)
 
             cur_max = max(node.val, node.val + left_val, node.val + right_val)
-            max_val = max(max_val, cur_max, node.val + left_val + right_val)
+            self.max_val = max(self.max_val, cur_max, node.val + left_val + right_val)
             return cur_max
 
         traverse_path(root)
-        return max_val
+        return self.max_val
