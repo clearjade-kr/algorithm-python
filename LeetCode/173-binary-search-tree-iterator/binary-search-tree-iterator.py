cc8e1a7cea9c67326e14ea1387a08d4e957c9925
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2024-06-09 17:11:54 +0900

    Time: 75 ms (6.21%) | Memory: 22 MB (88.72%) - LeetSync

--- LeetCode/173-binary-search-tree-iterator/binary-search-tree-iterator.py
+++ LeetCode/173-binary-search-tree-iterator/binary-search-tree-iterator.py
@@ -4,31 +4,28 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-def inorder_traversal(node):
-    result = []
-
-    if node.left:
-        result.extend(inorder_traversal(node.left))
-
-    result.append(node.val)
-
-    if node.right:
-        result.extend(inorder_traversal(node.right))
-
-    return result
-
-
 class BSTIterator:
     def __init__(self, root: Optional[TreeNode]):
-        self.iterator = inorder_traversal(root)
-        self.idx = -1
+        self.next_idx = 0
+
+        def inorder(node):
+            ret_nodes = []
+            if node.left:
+                ret_nodes.extend(inorder(node.left))
+            ret_nodes.append(node.val)
+            if node.right:
+                ret_nodes.extend(inorder(node.right))
+            return ret_nodes
+
+        self.list_nodes = inorder(root)
 
     def next(self) -> int:
-        self.idx += 1
-        return self.iterator[self.idx]
+        ret_val = self.list_nodes[self.next_idx]
+        self.next_idx += 1
+        return ret_val
 
     def hasNext(self) -> bool:
-        return self.idx + 1 < len(self.iterator)
+        return self.next_idx < len(self.list_nodes)
 
 # Your BSTIterator object will be instantiated and called as such:
 # obj = BSTIterator(root)
