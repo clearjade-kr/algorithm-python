8b3526cc7b5a66e59f21b9374b412b75ad4dc5c2
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2024-06-10 23:54:59 +0900

    Time: 63 ms (9.23%) | Memory: 21 MB (71.26%) - LeetSync

--- LeetCode/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ LeetCode/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,36 +7,16 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        from copy import copy
-        dict_nodes = {}
+        if not root:
+            return None
 
-        if p == q:
-            return p
+        if root == p or root == q:
+            return root
 
-        list_p = ""
-        list_q = ""
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
 
-        stack = [(root, "")]
-        while stack:
-            node, cur_list = stack.pop()
-            if not node:
-                continue
-            dict_nodes[node.val] = node
-            cur_list = cur_list + " " + str(node.val)
-            if node == p:
-                list_p = copy(cur_list)
-            if node == q:
-                list_q = copy(cur_list)
+        if left and right:
+            return root
 
-            stack.append((node.left, copy(cur_list)))
-            stack.append((node.right, copy(cur_list)))
-
-        list_p = list_p.split()
-        list_q = list_q.split()
-        min_len = min(len(list_p), len(list_q))
-        idx = 0
-        for i in range(min_len):
-            if list_p[i] != list_q[i]:
-                break
-            idx = i
-        return dict_nodes[int(list_p[idx])]
+        return left or right
