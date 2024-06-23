c6e7e7af87cf1f75f3ab10eaa09c4f4be0d5d0ff
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2024-06-23 20:02:50 +0900

    Time: 43 ms (40.60%) | Memory: 16.6 MB (90.37%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,15 +1,34 @@
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         def backtrack(path, nums):
+#             if len(path) == len(nums):
+#                 res.append(path)
+#                 return
+#             for i in range(len(nums)):
+#                 if nums[i] in path:
+#                     continue
+#                 backtrack(path + [nums[i]], nums)
+
+#         res = []
+#         backtrack([], nums)
+#         return res
+    
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(path, nums):
-            if len(path) == len(nums):
-                res.append(path)
-                return
-            for i in range(len(nums)):
-                if nums[i] in path:
-                    continue
-                backtrack(path + [nums[i]], nums)
+        if len(nums) == 1:
+            return [nums[:]]
 
         res = []
-        backtrack([], nums)
+
+        for _ in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for p in perms:
+                p.append(n)
+
+            res.extend(perms)
+            nums.append(n)
+
         return res
-    
