d0d47ecb2464bec74ffc9e96e73f4b6e32d94f7f
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2024-09-10 23:41:19 +0900

    Time: 205 ms (5.00%) | Memory: 17.3 MB (86.92%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -3,12 +3,14 @@ class Solution:
         start, end = 0, len(s) - 1
         vowels = 'aeiouAEIOU'
         while start < end:
-            if s[start] in vowels and s[end] in vowels:
+            while start < end and s[start] not in vowels:
+                start += 1
+            while start < end and s[end] not in vowels:
+                end -= 1
+
+            if start < end:
                 s = s[:start] + s[end] + s[start + 1:end] + s[start] + s[end + 1:]
                 start += 1
                 end -= 1
-            if s[start] not in vowels:
-                start += 1
-            if s[end] not in vowels:
-                end -= 1
+
         return s
