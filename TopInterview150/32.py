82ec8f2ccc1552fcde1785b286d32b1e4a1e820d
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2026-07-11 17:45:35 +0000

    Solved with O(n^2) solution, start - end check Found there is O(n) way solution

---
+++ TopInterview150/32.py
@@ -0,0 +1,36 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        # Since we cannot define from beginning, planning to use 2 dimension DP
+        # dp[i][j] = s[i:j] counting '(' as stack
+        # if dp[i][j-1] < 0 -> Not valid starting from i
+        
+        cur_max = 0
+        cnt_close = s.count(')')
+        for i in range(len(s)):
+            cur_stack = 0
+            for j in range(i, len(s)):
+                if s[j] == '(':
+                    if j > 0:
+                        cur_stack = cur_stack + 1 if cur_stack >= 0 else -1
+                    else:
+                        cur_stack = 1
+                else: # if s[j] == ')'
+                    if j > 0:
+                        cur_stack = cur_stack - 1 if cur_stack > 0 else -1
+                    else:
+                        cur_stack = -1
+
+                if cur_stack < 0:
+                    break
+                elif cur_stack > cnt_close:
+                    break
+                elif cur_stack == 0:
+                    cur_max = max(cur_max, j - i + 1)
+
+        return cur_max
+    
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = ")()())"
+    print(sol.longestValidParentheses(s=s))
