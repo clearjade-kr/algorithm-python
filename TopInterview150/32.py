dfd5084619591d83215fe86c3ac274c2734940d5
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2026-07-13 22:59:28 +0000

    Reviewed problem 32 to have O(N) time

--- TopInterview150/32.py
+++ TopInterview150/32.py
@@ -4,31 +4,58 @@ class Solution:
         # dp[i][j] = s[i:j] counting '(' as stack
         # if dp[i][j-1] < 0 -> Not valid starting from i
         
+        # cur_max = 0
+        # cnt_close = s.count(')')
+        # for i in range(len(s)):
+        #     cur_stack = 0
+        #     for j in range(i, len(s)):
+        #         if s[j] == '(':
+        #             if j > 0:
+        #                 cur_stack = cur_stack + 1 if cur_stack >= 0 else -1
+        #             else:
+        #                 cur_stack = 1
+        #         else: # if s[j] == ')'
+        #             if j > 0:
+        #                 cur_stack = cur_stack - 1 if cur_stack > 0 else -1
+        #             else:
+        #                 cur_stack = -1
+
+        #         if cur_stack < 0:
+        #             break
+        #         elif cur_stack > cnt_close:
+        #             break
+        #         elif cur_stack == 0:
+        #             cur_max = max(cur_max, j - i + 1)
+
+        # return cur_max
+
         cur_max = 0
-        cnt_close = s.count(')')
-        for i in range(len(s)):
-            cur_stack = 0
-            for j in range(i, len(s)):
-                if s[j] == '(':
-                    if j > 0:
-                        cur_stack = cur_stack + 1 if cur_stack >= 0 else -1
-                    else:
-                        cur_stack = 1
-                else: # if s[j] == ')'
-                    if j > 0:
-                        cur_stack = cur_stack - 1 if cur_stack > 0 else -1
-                    else:
-                        cur_stack = -1
+        left, right = 0, 0
+        for i in s:
+            if i == '(':
+                left += 1
+            else:
+                right += 1
 
-                if cur_stack < 0:
-                    break
-                elif cur_stack > cnt_close:
-                    break
-                elif cur_stack == 0:
-                    cur_max = max(cur_max, j - i + 1)
-
+            if left == right:
+                cur_max = max(cur_max, left + right)
+            elif right > left:
+                left = right = 0
+            
+        left, right = 0, 0
+        for i in reversed(s):
+            if i == ')':
+                right += 1
+            else:
+                left += 1
+            
+            if right == left:
+                cur_max = max(cur_max, left + right)
+            elif left > right:
+                left = right = 0
+            
         return cur_max
-    
+
 
 if __name__ == "__main__":
     sol = Solution()
