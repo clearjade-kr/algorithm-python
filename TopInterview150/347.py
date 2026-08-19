5389103f9528db299475dd065881f603a9ec5f89
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2026-08-19 21:23:03 +0000

    Solved with simple count and sort approach Checked better time complexity approach

---
+++ TopInterview150/347.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        from collections import defaultdict
+
+        dict_cnt = defaultdict(int)
+        for num in nums:
+            dict_cnt[num] += 1
+
+        list_cnt = list(dict_cnt.items())
+        list_cnt.sort(key = lambda x: x[1])
+
+        ret_list = []
+        for i in range(k):
+            ret_list.append(list_cnt[len(list_cnt) - 1 - i][0])
+        return ret_list
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [1,1,1,1]
+    k = 1
+    print(sol.topKFrequent(nums=nums, k=k))
