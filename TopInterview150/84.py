76be552debbae0d5d538ae7de66f79d26a1682cc
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2026-08-30 21:43:42 +0000

    Review of 4th week Aug

--- TopInterview150/84.py
+++ TopInterview150/84.py
@@ -3,36 +3,26 @@ from typing import List
 
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        # Save index closest to current and lower than current
-        N = len(heights)
-        left_lower = [-1] * N
-        right_lower = [N] * N
-
-        stack_left = []
-        stack_right = []
-        for i in range(N):
-            while stack_left and heights[stack_left[-1]] > heights[i]:
-                target_idx = stack_left.pop()
-                right_lower[target_idx] = i
-
-            while stack_right and heights[stack_right[-1]] > heights[N - 1 - i]:
-                target_idx = stack_right.pop()
-                left_lower[target_idx] = N - 1 - i
-
-            stack_left.append(i)
-            stack_right.append(N - 1 - i)
-
+        heights.append(0)
+        monotone_stack = [-1]
         ret_val = 0
+
         for idx, val in enumerate(heights):
-            cur_val = val * (right_lower[idx] - left_lower[idx] - 1)
-            ret_val = max(ret_val, cur_val)
+            while monotone_stack[-1] != -1 and val <= heights[monotone_stack[-1]]:
+                # Current height 
+                cur_height = heights[monotone_stack.pop()]
+                # Current width -> last index in stack
+                cur_width = idx - monotone_stack[-1] - 1
+                ret_val = max(ret_val, cur_height * cur_width)
+
+            monotone_stack.append(idx)
 
         return ret_val
 
 
 if __name__ == "__main__":
     sol = Solution()
-    # heights = [2, 1, 5, 6, 2, 3]
-    heights = [1]
+    heights = [2, 1, 5, 6, 2, 3]
+    # heights = [1]
     # heights = [2, 2]
     print(sol.largestRectangleArea(heights=heights))    
