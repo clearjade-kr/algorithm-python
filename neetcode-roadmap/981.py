3a00d2e55120298cc99148e762b84b3241c5715e
김재민 <70215942+clearjade-kr@users.noreply.github.com>
2026-09-03 01:06:39 +0000

    Solved with binary search on valid timestamp

---
+++ neetcode-roadmap/981.py
@@ -0,0 +1,37 @@
+class TimeMap:
+
+    def __init__(self):
+        # key -> (timestamp, value) pairs
+        self.store = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        # If the key doesn't exist, return empty string
+        values = self.store.get(key, [])
+        
+        left, right = 0, len(values) - 1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            
+            # If mid timestamp is smaller, keep it as return value
+            # Keep search for bigger timestamp
+            if values[mid][0] <= timestamp:
+                res = values[mid][1]   
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return res
+
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
